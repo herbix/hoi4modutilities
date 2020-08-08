04618de555381771175a783b8cdd95bda4abd9ea
Chaofan <herbix@163.com>
2020-08-08 16:17:24 +0800

    Add unit test framework

---
+++ test/src/extension.test.ts
@@ -0,0 +1,9 @@
+import * as assert from 'assert';
+// if you used the '@types/mocha' method to install mocha type definitions, uncomment the following line
+// import 'mocha';
+
+describe('Hello function', () => {
+    it('should return hello world', () => {
+        assert.equal(2, 1 + 1);
+    });
+});
