ce7ac31ac9fac2c626caf6d80f78e74d6ed7b42b
1985312383 <56398475+1985312383@users.noreply.github.com>
2026-08-19 15:38:19 +0800

    Update schema.test.ts

--- test/suite/focustree/schema.test.ts
+++ test/suite/focustree/schema.test.ts
@@ -1,10 +1,10 @@
 import * as assert from 'assert';
 import { parseHoi4File } from '../../../src/hoiformat/hoiparser';
-import { getFocusTree } from '../../../src/previewdef/focustree/schema';
+import { convertFocusFileNodeToJson } from '../../../src/previewdef/focustree/schema';
 
 suite('focus search filters', () => {
-    test('keeps search filters on regular, shared, and joint focuses', () => {
-        const focusTrees = getFocusTree(parseHoi4File(`
+    test('parses search filters on regular, shared, and joint focuses', () => {
+        const focusFile = convertFocusFileNodeToJson(parseHoi4File(`
             focus_tree = {
                 id = test_tree
                 focus = {
@@ -20,12 +20,11 @@ suite('focus search filters', () => {
                 id = TEST_JOINT_FOCUS
                 search_filters = { FOCUS_FILTER_INDUSTRY }
             }
-        `), [], 'test.txt');
+        `), {});
 
-        assert.deepStrictEqual(focusTrees.find(tree => tree.id === 'test_tree')?.focuses.TEST_FOCUS.searchFilters,
+        assert.deepStrictEqual(focusFile.focus_tree[0].focus[0].search_filters._values,
             ['FOCUS_FILTER_POLITICAL', 'FOCUS_FILTER_INNER_CIRCLE']);
-        const sharedFocuses = focusTrees.find(tree => tree.isSharedFocues)?.focuses;
-        assert.deepStrictEqual(sharedFocuses?.TEST_SHARED_FOCUS.searchFilters, ['FOCUS_FILTER_RESEARCH']);
-        assert.deepStrictEqual(sharedFocuses?.TEST_JOINT_FOCUS.searchFilters, ['FOCUS_FILTER_INDUSTRY']);
+        assert.deepStrictEqual(focusFile.shared_focus[0].search_filters._values, ['FOCUS_FILTER_RESEARCH']);
+        assert.deepStrictEqual(focusFile.joint_focus[0].search_filters._values, ['FOCUS_FILTER_INDUSTRY']);
     });
 });
